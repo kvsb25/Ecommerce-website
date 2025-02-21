d3206265a42116574c5d69321040ea023c37c3d3
Kovidh <kovidhvsbhati@gmail.com>
2025-02-21 14:15:47 +0530

    added routes for user authentication

--- server/app.js
+++ server/app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
-// const userRouter = require("./routes/user.js");
+const userRouter = require("./routes/user.js");
 const customerRouter = require("./routes/customer.js");
 const vendorRouter = require("./routes/vendor.js");
 const ordersRouter = require("./routes/order.js");
 const generalPlatformRouter = require("./routes/generalPlatform.js");
 
 app.use("/", generalPlatformRouter);
-// app.use("/user", userRouter);
+app.use("/user", userRouter);
 app.use("/customer", customerRouter); //routes imitating a different platform, customer specific platform 
 app.use("/vendor", vendorRouter); //routes imitating a different platform, vendor specific platform
 app.use("/order", ordersRouter);
--- server/routes/user.js
+++ server/routes/user.js
@@ -3,7 +3,24 @@ const router = express.Router();
 const customerRouter = require("./customer.js");
 const vendorRouter = require("./vendor.js");
 
-router.use('/', customerRouter);
-router.use('/', vendorRouter);
+router.route("/signup")
+    .get((req, res)=>{
+        res.send('signup form')
+    })
+    .post((req, res)=>{
+        // register user to database
+        // create a jwt token of the user and send it in a httpOnly cookie
+        res.send('httpStatus');
+    })
+
+router.route('/login')
+    .get((req, res)=>{
+        // if jwt in cookie is verified then redirect to the page previous page
+        res.send('login form')
+    })
+    .post((req, res)=>{
+        // login user by
+        res.send('httpStatus');
+    })
 
 module.exports = router;
